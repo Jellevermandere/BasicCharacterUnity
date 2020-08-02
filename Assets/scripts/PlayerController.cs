8bbc22475d718f2599340237d8c1f89b21598320
Jelle Vermandere <jelle.vermandere@gmail.com>
2020-08-02 11:41:17 +0200

    init
    init

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    [SerializeField]
+    private float movementSpeed, rotationSpeed, jumpSpeed, gravity;
+
+    private Vector3 movementDirection = Vector3.zero;
+    private bool playerGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerGrounded = characterController.isGrounded;
+
+        //movement
+        Vector3 inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
+        characterController.Move(inputMovement * Time.deltaTime);
+
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
+
+
+        //jumping
+        if(Input.GetButton("Jump") && playerGrounded)
+        {
+            movementDirection.y = jumpSpeed;
+        }
+        movementDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(movementDirection * Time.deltaTime);
+
+
+        //animations
+        animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0);
+        animator.SetBool("isJumping", !characterController.isGrounded);
+
+    }
+}
---
+++ Assets/scripts/TestScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(CharacterController))]
+public class TestScript : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Animator animator;
+
+    [SerializeField]
+    private float movementSpeed, rotationSpeed, jumpSpeed, gravity;
+
+    private Vector3 movementDirection = Vector3.zero;
+    private bool playerGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerGrounded = characterController.isGrounded;
+       
+        Vector3 move = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
+        characterController.Move(move * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
+        }
+
+
+        if (Input.GetButton("Jump") && playerGrounded)
+        {
+            movementDirection.y = jumpSpeed;
+        }
+        movementDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(movementDirection * Time.deltaTime);
+        
+        animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0);
+        animator.SetBool("isJumping", !characterController.isGrounded);
+        Debug.Log(characterController.isGrounded);
+        
+    }
+}
